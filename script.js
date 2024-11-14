d09baa6e37d3db2aee6a190cfb766a9d12aaa27d
ArrayArefin-07 <mobasherarefint2001@gmail.com>
2024-11-14 06:08:30 +0600

    Added functionality for create password
    And Show created password in frontend

--- script.js
+++ script.js
@@ -5,3 +5,21 @@ const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerCase = "abcdefghijklmnopqrstuvwxyz";
 const numbers = "0123456789";
 const symbols = "!@#$%^&*()_+-={}:<>?";
+
+const allChars =upperCase + lowerCase + numbers + symbols;
+
+function createPassword() {
+  let password = "";
+  password += upperCase[Math.floor(Math.random() * upperCase.length)];
+  password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+  password += numbers[Math.floor(Math.random() * numbers.length)];
+  password += symbols[Math.floor(Math.random() * symbols.length)];
+
+  while(length > password.length){
+    password += allChars[Math.floor(Math.random() * allChars.length)];
+    console.log(password);
+  }
+  passwordBox.value = password;
+}
+
+// createPassword();
